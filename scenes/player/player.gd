7dc0351f0676c172f12f735776419dd5ced26444
Adryan <Douuuradodev@gmail.com>
2026-04-29 00:31:40 -0300

    Desenvolvimento arma

---
+++ scenes/player/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+var delta_1 = 0.0
+@onready var gun = $Gun
+
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+	if direction != Vector2.ZERO:		
+		velocity.x = direction.x * SPEED
+		velocity.y = direction.y * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	move_and_slide()
+	
+@warning_ignore("unused_parameter")
+func _process(delta):
+	if Input.is_action_pressed("Bullet_shoot"):
+		gun.shoot()
